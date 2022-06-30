981f43f9bcb08855ca5e6958de51337dddd150ca
Sivalatha800 <rachamallusivalatha.com>
2022-06-30 14:49:07 +0530

    Added search results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,47 @@
 import React from "react";
 import MainPageLayout from "../components/MainPageLayout";
 import { useState } from "react";
+import { apiGet } from "../misc/config";
 
 function Home() {
   const [input, setInput] = useState("");
+  const [results, setResults] = useState(null);
+
   const onInputChange = (ev) => {
     setInput(ev.target.value);
   };
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then((r) => r.json())
-      .then((result) => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then((result) => {
+      setResults(result);
+    });
   };
 
   const onKeyDown = (ev) => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return (
+        <div>
+          <h3>No Results</h3>
+        </div>
+      );
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map((item) => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -33,6 +55,7 @@ function Home() {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 }
