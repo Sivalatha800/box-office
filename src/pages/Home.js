694f04d0115fdedd958d469e814775737681f958
Sivalatha800 <rachamallusivalatha.com>
2022-06-30 19:31:21 +0530

    Added Styles for grid and cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import MainPageLayout from "../components/MainPageLayout";
 import { useState } from "react";
 import { apiGet } from "../misc/config";
+import ShowGrid from "../components/show/ShowGrid";
+import ActorGrid from "../components/actor/ActorGrid";
 
 function Home() {
   const [input, setInput] = useState("");
@@ -39,11 +41,11 @@ function Home() {
       );
     }
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map((item) => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map((item) => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
 
     return null;
